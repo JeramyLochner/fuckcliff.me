de3b9efe9caa855a8e07ecacbb3450f3409a1cb9
leeleegabriel <lgv0001@auburn.edu>
2019-07-14 15:53:37 -0500

    switched to express

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
 import sirv from 'sirv';
-import polka from 'polka';
+// import polka from 'polka';
+import express from 'express'
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, IP, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+// polka() // You can also use Express
+// 	.use(
+// 		compression({ threshold: 0 }),
+// 		sirv('static', { dev }),
+// 		sapper.middleware()
+// 	)
+// 	.listen('3000', err => {
+// 		if (err) console.log('error', err);
+// 	});
+
+express() // or Polka, or a similar framework
 	.use(
+		'/', //
 		compression({ threshold: 0 }),
+		//serve('assets'),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-	.listen('3000', err => {
-		if (err) console.log('error', err);
-	});
+	.listen(process.env.PORT, process.env.IP);
